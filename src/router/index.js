a81cac936633514b323ff360f66bc84f8af00907
瀬名波哲矢 <>
2020-05-29 14:59:56 +0900

    vuetifyとrouterの導入

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Tokyo from '../views/Tokyo.vue'
+import Wards from '../views/Wards.vue'
+import HelloWorld from '../components/HelloWorld';
+
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: '/',
+		name: 'Tokyo',
+		component: Tokyo
+	},
+	{
+		path: '/23wards',
+		name: '23wards',
+		component: Wards
+	},
+	{
+		path: '/hello',
+		name: 'Hello',
+		component: HelloWorld
+	}
+]
+
+const router = new VueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
+})
+
+export default router
